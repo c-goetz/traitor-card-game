876396dfc819c0f99ea5e833f2c3e61f92a03b72
Carlo Goetz <carlo.goetz@googlemail.com>
2022-06-13 23:24:15 +0200

    serve static and index template

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
-package traitor_card_game
+package main
 
-import "fmt"
+import (
+	"bytes"
+	"embed"
+	"html/template"
+	"io"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed static/htmx.min.js
+var htmx []byte
+
+//go:embed templates
+var templates embed.FS
 
 func main() {
-	fmt.Print("hi")
+	tsFS, err := fs.Sub(templates, "templates")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ts := template.Must(template.ParseFS(tsFS, "*.html"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/static/htmx.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/javascript")
+		reader := bytes.NewReader(htmx)
+		io.Copy(w, reader)
+	})
+	mux.HandleFunc("/index.html", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		ts.ExecuteTemplate(w, "index.html", nil)
+	})
+	err = http.ListenAndServe(":8000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
